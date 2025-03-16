152415346e820ef3f0193c99acc4c03cae8f7ecc
eWeb0 <opdahlethan00@gmail.com>
2025-03-16 12:46:28 -0600

    add tailwind css

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div >
+
+      <main className="text-red-600">
+      <p>Placeholder</p>
+      </main>
+
+      <footer>
+       <p>PlaceHolder</p>
+      </footer>
+    </div>
+  );
+}
